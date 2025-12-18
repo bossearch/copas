152c3cfffb6b245bc06b1053d398dae0a6eb54a7
bossearch <yudha.bosse@gmail.com>
2025-12-19 04:32:47 +0800

    feat: add nlohmann_json and cpp_httplib to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,23 +1,17 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    gcc
     cmake
     pkg-config
     gtk3
     libayatana-appindicator
     wl-clipboard
     nlohmann_json
-    curl
-    iproute2
-    inetutils
-    glib
-    sysprof
+    httplib
   ];
 
   shellHook = ''
     export PKG_CONFIG_PATH="${pkgs.sysprof}/lib/pkgconfig:$PKG_CONFIG_PATH"
     export LD_LIBRARY_PATH="${pkgs.libayatana-appindicator}/lib:$LD_LIBRARY_PATH"
-    echo "copas dev shell ready"
   '';
 }
